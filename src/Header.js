740579ff6e3275aaa4b369a17ef4cc506f25b875
Re1d0n <reiduninnes@gmail.com>
2022-07-26 20:48:23 -0700

    added header footer and tabs

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <Header>
+      <h3>
+        Thanks for visiting my Portfolio. Feel free to contact me using the
+        contact info provided.
+      </h3>
+      
+    </Header>
+  );
+}
+
+export default Header;
---
+++ src/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h3>
+        Thanks for visiting my Portfolio! Feel free to contact me using the
+        contact information provided.
+      </h3>
+      
+    </footer>
+  );
+}
+
+export default Footer;
